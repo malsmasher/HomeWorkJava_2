308ebb82cc42afae0d68cebd4d216f9e54d33642
m.malyshonok <masha.malyshonok@gmail.com>
2024-05-28 11:41:03 +0500

    Задача 2

--- src/Nomber_2.java
+++ src/Nomber_2.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Nomber_2 {
+public class Nomber_2 {
+    public static void main(String[] args) {
+
+        int x = 50;
+        int y = 3000;
+        if (y > 1000) {
+            System.out.println("Итоговый счёт:");
+            System.out.println(x + y + y/100*1);
+
+            System.out.println("Начислено бонусов:");
+            System.out.println(y/100*1);
+        }
+    }
 }
+git 
